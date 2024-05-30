fc97541bec87c59238b7bbdf8ec63d0fb8eaa81b
Maziar Sojoudian <maziar@sojoudian.net>
2024-05-30 18:06:24 -0400

    add first function

---
+++ week5/app1/go.mod
@@ -0,0 +1,3 @@
+module github.com/sojoudian/app1
+
+go 1.22.3
---
+++ week5/app1/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	greeting()
+}
+
+// https://go.dev/play/
+func greeting() {
+	fmt.Print("Hello world!\n")
+}
+
+// func functionName() {
+// 	Block of code
+// }
