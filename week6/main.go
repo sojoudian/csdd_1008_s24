2932d4dd3be4e7988b953f94345fa081bb08546b
Maziar Sojoudian <maziar@sojoudian.net>
2024-06-06 18:11:13 -0400

    add the about page

--- week6/main.go
+++ week6/main.go
@@ -13,10 +13,15 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Welcome to the Go API Server!")
 }
 
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "This is the About page.")
+}
+
 // go mod init github.com/sojoudian/SimpleAPIServer
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", homeHandler)
+	mux.HandleFunc("/about", aboutHandler)
 
 	log.Println("Starting server on :3001")
 	err := http.ListenAndServe(":3001", mux)
