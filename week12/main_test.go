a802af94b7c1c6bb44ba7dda075b7813b7814be5
Maziar Sojoudian <maziar@sojoudian.net>
2024-07-18 17:50:34 -0400

    add test for NotFound situation

--- week12/main_test.go
+++ week12/main_test.go
@@ -23,3 +23,15 @@ func TestHomeHandler(t *testing.T) {
 		t.Errorf("Expected body %q; got %q", expected, rec.Body.String())
 	}
 }
+
+func TestHomeHandler_NotFound(t *testing.T) {
+	req, err := http.NewRequest("GET", "/notfound", nil)
+	if err != nil {
+		t.Fatalf("Could not create request: %v", err)
+	}
+	rec := httptest.NewRecorder()
+	homeHandler(rec, req)
+	if rec.Code != http.StatusNotFound {
+		t.Errorf("Expected status NotFound; go %v", rec.Code)
+	}
+}
