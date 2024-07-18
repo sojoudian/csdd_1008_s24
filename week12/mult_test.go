8b89e858d0aba2e9ee6337c98ae060dba5e5edb1
Maziar Sojoudian <maziar@sojoudian.net>
2024-07-18 18:14:53 -0400

    add Mult and Mult_test.go

---
+++ week12/mult_test.go
@@ -0,0 +1,24 @@
+package main
+
+import "testing"
+
+func TestMult(t *testing.T) {
+	testCases := []struct {
+		a, b     int
+		expected int
+	}{
+		{2, 3, 6},
+		{2, 39, 78},
+		{2, 0, 0},
+		{2, -2, -4},
+		{-2, -2, 4},
+	}
+
+	for _, tc := range testCases {
+		result := Mult(tc.a, tc.b)
+		if result != tc.expected {
+			t.Errorf("Lult(%d, %d) = %d; expected %d", tc.a, tc.b, result, tc.expected)
+		}
+	}
+
+}
